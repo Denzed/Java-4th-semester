8ca53a38bbcc3a358c6834b01fd9eaa995f96702
Daniil Smirnov (aka Denzed) <dansmi97@gmail.com>
2017-03-01 00:31:21 +0300

    Refined JavaDoc and added (what in my opinion it is) standalone jar

--- src/main/java/lazy/LazyFactory.java
+++ src/main/java/lazy/LazyFactory.java
@@ -3,38 +3,40 @@ package lazy;
 import java.util.function.Supplier;
 
 /**
- * Class for generation {@link lazy.Lazy} implementations
+ * Class for generation of various {@link lazy.Lazy} implementations
  */
-public class LazyFactory {
+public final class LazyFactory {
+    private LazyFactory() {
+    }
 
     /**
-     * Generates a {@link lazy.LazySingleThreaded} -- single threaded implementation of {@link lazy.Lazy}
+     * Generates an instance of {@link lazy.LazySingleThreaded} -- single threaded implementation of {@link lazy.Lazy}
      *
      * @param supplier Calculation to store
      * @param <T>      Return type
-     * @return Resulting {@link lazy.Lazy}
+     * @return the instance of single threaded implementation of {@link lazy.Lazy}
      */
     public static <T> Lazy<T> createLazySingleThreaded(Supplier<T> supplier) {
         return new LazySingleThreaded<>(supplier);
     }
 
     /**
-     * Generates a {@link lazy.LazyMultiThreaded} -- thread-safe implementation of {@link lazy.Lazy}
+     * Generates an instance of  {@link lazy.LazyMultiThreaded} -- thread-safe implementation of {@link lazy.Lazy}
      *
      * @param supplier Calculation to store
      * @param <T>      Return type
-     * @return Resulting {@link lazy.Lazy}
+     * @return the instance of thread-safe implementation of {@link lazy.Lazy}
      */
     public static <T> Lazy<T> createLazyMultiThreaded(Supplier<T> supplier) {
         return new LazyMultiThreaded<>(supplier);
     }
 
     /**
-     * Generates a {@link lazy.LazyLockFree} thread-safe lock-free implementation of {@link lazy.Lazy}
+     * Generates an instance of  {@link lazy.LazyLockFree} -- thread-safe lock-free implementation of {@link lazy.Lazy}
      *
      * @param supplier Calculation to store
      * @param <T>      Return type
-     * @return Resulting {@link lazy.Lazy}
+     * @return the instance of thread-safe lock-free implementation of  {@link lazy.Lazy}
      */
     public static <T> Lazy<T> createLazyLockFree(Supplier<T> supplier) {
         return new LazyLockFree<>(supplier);
